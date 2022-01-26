de11f3ef08901c06c06822f44dd045d624261dcb
mssabr01 <sabraoui.m@gmail.com>
2022-01-26 11:52:08 -0500

    added ability to add new people and dirt

--- dbs_gui.py
+++ dbs_gui.py
@@ -2,11 +2,19 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from dbs_helpers import *
+from tkinter import filedialog
+
+dirt_json = "./files/dirttree.json"
 
 # root window
 root = tk.Tk()
 root.geometry('500x500')
 root.title('Dead Body Sonr')
+
+def pickdir():
+    dirtydir = filedialog.askdirectory()
+    dir_var.set(dirtydir)
+
 
 # create a notebook
 notebook = ttk.Notebook(root)
@@ -23,13 +31,26 @@ homeframe.pack(fill='both', expand=True)
 
 #Dirt Frame
 person_var = tk.StringVar()
+person_label = tk.Label(dirtframe, text = "Person").grid(row=0, column=0)
+person_entry = tk.Entry(dirtframe, textvariable = person_var).grid(row=0, column=1)
+
 dir_var = tk.StringVar()
-person_entry = tk.Entry(homeframe)
-person_entry.pack()
-dir_entry = tk.Entry(root,textvar = dir_var)
-dirttree = ttk.Treeview(dirtframe)
-dirttree = populate_dirttree(filepath="./files/dirttree.json",treeview=dirttree)
-dirttree.pack()
+dir_btn = Button(dirtframe, text ='Choose Directory', command = lambda:pickdir()).grid(row=1,column=0)
+dir_entry = tk.Entry(dirtframe, textvariable=dir_var, state=DISABLED).grid(row=1, column=1)
+
+global dirttree
+dirttree = ttk.Treeview(dirtframe, column='c1')
+dirttree = populate_dirttree(filepath=dirt_json,treeview=dirttree).grid(row=3, columnspan=3)
+add_btn = Button(dirtframe, 
+                 text = 'Add dirty person', 
+                 command = lambda:add_n_refresh_tree()
+                 ).grid(row=2,column=0)
+
+def add_n_refresh_tree():
+    #dirttree.grid_forget()
+    dirttree = ttk.Treeview(dirtframe, column='c1')
+    add_dirty_person(name=person_var.get(), dirtpath=dir_var.get(), filepath=dirt_json)
+    dirttree = populate_dirttree(filepath=dirt_json, treeview=dirttree).grid(row=3,columnspan=3)
 
 # add frames to notebook
 
--- dbs_helpers.py
+++ dbs_helpers.py
@@ -1,15 +1,40 @@
 import json
+import uuid
+import os
+
+def add_dirty_person(name, dirtpath, filepath):
+    ##
+    # Adds a person to the dirt json file
+    ##
+    id = str(uuid.uuid4())
+
+    #get current dirt tree
+    f = open(filepath, 'r')
+    #handle empty file
+    if(os.stat(filepath).st_size == 0):
+        data = []
+    else:
+        data = json.load(f)
+    f.close()
+
+    data.append({'id' : id, 'name' : name, 'dirtdir' : dirtpath})
+    
+    with open(filepath, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 def populate_dirttree(filepath, treeview):
     ##
     # Populates the treeview with names and dirt directories
     ##
     
-    #open file 
-    f = open(filepath)
-    data = json.load(f)
+    #handle empty file
+    if(os.stat(filepath).st_size == 0):
+        return treeview
 
-    i = 0
+    #open file 
+    f = open(filepath, 'r')
+    data = json.load(f)
+    
     for person in data:
         treeview.insert('', '1', person["id"], text = person["name"])
         treeview.insert(person["id"], '1', person["id"] + '_dir', text = person["dirtdir"])
